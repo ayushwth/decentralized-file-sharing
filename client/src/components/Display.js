5f7a7658e2b63d350d20619ad54fea82337ba423
ayushwth <ayushsrivastavax@gmail.com>
2026-03-25 07:41:55 +0530

    crying at this point

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import "./Display.css";
-const Display = ({sendCountToParent, contract, account }) => {
+const Display = ({ contract, account }) => {
   const [data, setData] = useState("");
-  const [place,setPlace]=useState(true);
   const getdata = async () => {
     let dataArray;
     const Otheraddress = document.querySelector(".address").value;
@@ -23,14 +22,13 @@ const Display = ({sendCountToParent, contract, account }) => {
       const str_array = str.split(",");
       // console.log(str);
       // console.log(str_array);
-      sendCountToParent(str_array.length);
       const images = str_array.map((item, i) => {
         return (
-          <a href={item} key={i} target="_blank" rel="noreferrer">
+          <a href={item} key={i} target="_blank">
             <img
               key={i}
               src={`https://gateway.pinata.cloud/ipfs/${item.substring(6)}`}
-              alt={i+1}
+              alt="new"
               className="image-list"
             ></img>
           </a>
@@ -46,12 +44,10 @@ const Display = ({sendCountToParent, contract, account }) => {
       <div className="image-list">{data}</div>
       <input
         type="text"
-        placeholder={place?"Enter Address":""}
+        placeholder="Enter Address"
         className="address"
-        onFocus={()=>{setPlace(false);}}
-        onBlur={()=>{setPlace(true)}}
       ></input>
-      <button className="center button" onClick={()=>{getdata()}}>
+      <button className="center button" onClick={getdata}>
         Get Data
       </button>
     </>
